dbd8683e830edbda01a0cacb3c63a3856bae134f
tkts0426 <h.taka19980426@gmail.com>
2023-07-13 21:59:28 +0900

    add C#

---
+++ C#/3.変数と型/Program.cs
@@ -0,0 +1,41 @@
+﻿// このプログラムの説明を書く．
+
+namespace sample{
+
+    class Program{
+
+         enum Season{ 
+                Spring, Summer, Fall, Winter
+            };
+
+        static void Main(){
+            
+            // System.Console.WriteLine("Hello, World from C#");
+
+            // // memo: 匿名化
+            // var a = new { Name = "hirano", Age = 18 };
+            // System.Console.WriteLine(a.Age);
+
+            // problem 2
+            int x = 123;
+            float y = 1.23F;
+            System.Console.WriteLine(x);
+            System.Console.WriteLine(y);
+
+            // Problem 3
+            string money = @"\1,000";
+            System.Console.WriteLine(money);
+            string money_ = "\\23,000";
+            // string s_ = "\23,000"; <- これだとエラーがでる．
+            System.Console.WriteLine(money_);
+
+            // Problem 4
+            System.Console.WriteLine(Season.Spring);
+            System.Console.WriteLine((int)Season.Spring);
+
+            // Problem 5
+            var s = new [] { "春", "夏", "秋", "冬" };
+            System.Console.WriteLine(s[(int) Season.Spring]);
+        }
+    }
+}
---
+++ C#/4.式と演算子/Program.cs
@@ -0,0 +1,19 @@
+﻿// このプログラムの説明を書く．
+
+namespace sample{
+
+    class Program{
+
+        static void Main(){
+            
+            int x = 10;
+            int y = 20;
+
+            System.Console.WriteLine( (x+y) * (x+y) );
+
+            int z = 9;
+            string a = ( z%2 == 0 )? "even" : "odd";
+            System.Console.WriteLine( a );
+        }
+    }
+}
